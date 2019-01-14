ace0a99971d1a02cc01463497a1fcc07d8be35ab
Ray Bergholm <raymond.bergholm@sanoma.com>
2019-01-14 16:16:34 +0200

    add read_file and save_file

---
+++ file_handler.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def read_file(filepath, line_delimiter=None):
+    with open(filepath, "r") as file_stream:
+        data = file_stream.read()
+    
+    return data.split(line_delimiter) line_delimiter != None else data
+
+def save_file(filepath, data):
+    with open(filepath, "w+") as file_stream:
+        for entry in data:
+            file_stream.write(entry)
+
+    return True
