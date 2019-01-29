f8348f4125e2ca77a4b858b40c4b681d15a5ed77
Ray Bergholm <nalamint@gmail.com>
2019-01-29 15:06:05 +0200

    fix JSON file read and conversion bugs

--- json_handler.py
+++ json_handler.py
@@ -4,18 +4,18 @@ import json
 
 def json_to_csv(data, field_list=None): 
     # if no field_list is supplied, it will build it from the first item in the list. Doing it this way means that the key order will vary!
-    header = [key for key in data[0].keys()] if field_list != None else field_list
+    header = [key for key in data[0].keys()] if field_list == None else field_list
 
     json_data = []
     for line in data:
-        entry = {field: data[index] for index, field in enumerate(header)}
-        json_data.append(entry)
+        entry = [line[field] for field in header]
+        json_data.append("%s\n" % ";".join(entry))
     return json_data
 
 def from_json_file(filepath, field_list=None): # supply a field list to filter the JSON data, otherwise it will return the whole body as-is
     data = None
     with open(filepath, newline="") as file_stream:
-        data = json.loads(file_stream)
+        data = json.loads(file_stream.read())
 
         if field_list:
             filtered = []
