494217096fec830cae0bca053fda654ce9aa9a50
Ray Bergholm <raybergholm@gmail.com>
2019-03-12 15:35:48 +0200

    formatted file_handler

--- file_handler.py
+++ file_handler.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
+
 def read_file(filepath, line_delimiter=None):
     with open(filepath, "r") as file_stream:
         data = file_stream.read()
-    
-    return data.split(line_delimiter) line_delimiter != None else data
+
+    return data.split(line_delimiter) if line_delimiter != None else data
+
 
 def save_file(filepath, data):
     with open(filepath, "w+") as file_stream:
