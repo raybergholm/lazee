3a3413475dfeb05d9db0897cc5445fe0d8d5b076
Ray Bergholm <raybergholm@gmail.com>
2019-03-12 15:35:30 +0200

    bugfix json_to_csv

--- json_handler.py
+++ json_handler.py
@@ -2,17 +2,21 @@
 
 import json
 
-def json_to_csv(data, field_list=None): 
+
+def json_to_csv(data, field_list=None, delimiter=";"):
     # if no field_list is supplied, it will build it from the first item in the list. Doing it this way means that the key order will vary!
-    header = [key for key in data[0].keys()] if field_list == None else field_list
+    header = [key for key in data[0].keys(
+    )] if field_list == None else field_list
 
-    json_data = []
+    csv_data = []
     for line in data:
-        entry = [line[field] for field in header]
-        json_data.append("%s\n" % ";".join(entry))
-    return json_data
+        entry = [str(line[field]) if field in line else "" for field in header]
+        csv_data.append("%s\n" % delimiter.join(entry))
+    return csv_data
 
-def from_json_file(filepath, field_list=None): # supply a field list to filter the JSON data, otherwise it will return the whole body as-is
+
+# supply a field list to filter the JSON data, otherwise it will return the whole body as-is
+def from_json_file(filepath, field_list=None):
     data = None
     with open(filepath, newline="") as file_stream:
         data = json.loads(file_stream.read())
@@ -20,10 +24,12 @@ def from_json_file(filepath, field_list=None): # supply a field list to filter t
         if field_list:
             filtered = []
             for entry in data:
-                filtered_entry = {entry for key, value in data.items() if entry in field_list}
+                filtered_entry = {entry for key,
+                                  value in data.items() if entry in field_list}
                 filtered.append(filtered_entry)
 
     return data
+
 
 def to_json_file(filepath, data, field_list=None):
     with open(filepath, mode="w+", newline="") as file_stream:
